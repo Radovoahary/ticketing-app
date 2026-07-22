8754d6bbfcedd8df279650d5b306c24a5cf0d50a
radovoahary <voaharyradoniaina@gmail.com>
2026-07-22 22:33:20 +0300

    Changing another function

--- database/migrations/2026_07_22_192905_create_ticket_categories_table.php
+++ database/migrations/2026_07_22_192905_create_ticket_categories_table.php
@@ -10,12 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('ticket_categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('ticket_categories', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('event_id')->constrained()->cascadeOnDelete();
+        $table->string('type'); // Stockera la valeur de l'Enum (standard, vip, student)
+        $table->string('name'); // Nom d'affichage ex: "Place VIP Premier Rang"
+        $table->decimal('base_price', 8, 2); // Prix hors promotion / stratégie
+        $table->integer('capacity'); // Capacité totale
+        $table->integer('reserved_count')->default(0); // Capacité actuellement réservée
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
