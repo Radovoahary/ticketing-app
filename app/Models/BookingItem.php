c7f1c1d23fa04005d0aec01fb1f7751c002ea4d4
radovoahary <voaharyradoniaina@gmail.com>
2026-07-22 22:36:28 +0300

    BookingItem's model

--- app/Models/BookingItem.php
+++ app/Models/BookingItem.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BookingItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['booking_id', 'ticket_category_id', 'quantity', 'unit_price', 'subtotal'];
+
+    public function booking(): BelongsTo
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(TicketCategory::class, 'ticket_category_id');
+    }
 }
